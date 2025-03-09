e80d69105e6b644f64f48582c8230577e5c1aaa1
Dragon-Praragon <wojtek0_0@wp.pl>
2025-03-09 03:21:28 +0100

    another fix for roulette

--- src/events/client/resetLives.js
+++ src/events/client/resetLives.js
@@ -3,11 +3,12 @@ const { resetLives } = require('../../functions/handlers/handleRoulette');
 const logger = require('../../logger');
 
 // Schedule the resetLives function to run at midnight every day
-cron.schedule('0 0 * * *', async () => {
+cron.schedule('0 */12 * * *', async () => {
     try {
         await resetLives();
         logger.info('Lives reset successfully.');
     } catch (error) {
         logger.error(`Error resetting lives: ${error}`);
     }
+    logger.info('Cron job finished for resetting lives.');
 });
